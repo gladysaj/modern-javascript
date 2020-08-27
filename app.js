dc9378b537cfdcc57f3680de3e66cb06fef070d1
Gladys Alvarez <glad.alvarez1@gmail.com>
2020-08-26 23:21:59 -0500

    switches

--- app.js
+++ app.js
@@ -531,4 +531,46 @@ if(id === 105)
 else 
   console.log('INCORRECT');
 
-  
+
+// SWITCHES (another way to validate conditions)
+// Use this instead of plenty of else if's
+const colored = 'blue';
+
+switch(colored){
+  case 'red':
+    console.log('Color is red');
+    break;
+  case 'blue':
+    console.log('Color is blue');
+    break;
+  default:
+    console.log('Color is not red or blue')
+};
+
+// GET DAY OF THE WEEK
+
+let day;
+
+switch(new Date().getDay()){
+  case 0:
+    day = 'Sunday';
+    break;
+  case 1:
+    day = 'Monday';
+    break;
+  case 2:
+    day = 'Tuesday';
+    break;
+  case 3:
+    day = 'Thursday';
+    break;
+  case 4:
+    day = 'Friday';
+    break;
+  case 5:
+    day = 'Saturday';
+    break;
+}
+
+console.log(`Today is ${day}`);
+
