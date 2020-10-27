5353da0dc8d14c1e6f3e5b27958480ac74c21c25
Gladys Alvarez <glad.alvarez1@gmail.com>
2020-10-26 21:46:44 -0600

    Mouse events

--- app.js
+++ app.js
@@ -1142,6 +1142,50 @@ Method = a function inside of an object
 //  }
 
 
+// MOUSE EVENTS 
+
+// const clearBtn = document.querySelector('.clear-tasks');
+// const card = document.querySelector('.card');
+// const heading = document.querySelector('h5');
+
+// Click 
+// clearBtn.addEventListener('click', runEvent);
+
+// Doubleclick
+// clearBtn.addEventListener('dblclick', runEvent);
+
+// Mouse down 
+// clearBtn.addEventListener('mousedown', runEvent);
+
+// Mouse up 
+// clearBtn.addEventListener('mouseup', runEvent);
+
+// Mouse enter 
+// card.addEventListener('mouseenter', runEvent);
+
+// Mouse leave 
+// card.addEventListener('mouseleave', runEvent);
+
+// Mouse over 
+// card.addEventListener('mouseover', runEvent);
+
+// Mouse out 
+// card.addEventListener('mouseout', runEvent);
+
+// Mouse move (any movement inside the element)
+// card.addEventListener('mousemove', runEvent);
+
+
+// Event Handler 
+// function runEvent(e) {
+//   console.log(`EVENT TYPE: ${e.type}`);
+
+//   heading.textContent = `MouseX: ${e.offsetX} and MouseY: ${e.offsetY}`;
+
+//   document.body.style.backgroundColor = `rgb(${e.offsetX}, ${e.offsetY}, 40)`;
+// };
+
+
 
 
 
